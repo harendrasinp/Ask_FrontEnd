cc07733597e61278e6871d33e6c9434331e639ca
unknown <haren111990@gmail.com>
2025-10-20 22:17:35 +0530

    update header

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 export const Header = () => {
     return (
-        <div className='w-full bg-cyan-950 flex items-center justify-between gap-4 border-2 px-6'>
-            <div><Image src="/Images/Logo/logo1.png" alt="logo" width={100} height={0} className='rounded-full'/></div>
-            <div className='w-[25rem] flex justify-center items-center gap-6'>
-                <Link href="/" className='bg-blue-300 p-2 px-4 '>Home</Link>
-                <Link href="/about"className='bg-blue-300 p-2 px-4 '>About</Link>
-                <Link href="/contact"className='bg-blue-300 p-2 px-4 '>Contact</Link>
-                <Link href="/services"className='bg-blue-300 p-2 px-4 '>Services</Link>
+        <div className='w-full bg-cyan-950 flex items-center justify-between p-2'>
+            <div><Image src="/Images/Logo/logo1.png" alt="logo" width={80} height={0} className='rounded-full'/></div>
+            <div className='w-[15rem] flex justify-center items-center gap-2'>
+                <Link href="/" className='p-1 text-amber-50'>Home</Link>
+                <Link href="/about" className='p-1 text-amber-50'>About</Link>
+                <Link href="/contact" className='p-1 text-amber-50'>Contact</Link>
+                <Link href="/services" className='p-1 text-amber-50'>Services</Link>
             </div>
         </div>
     )
