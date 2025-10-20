4b35d40fd3abf2fa7560b6adf545ad200d1c330f
unknown <haren111990@gmail.com>
2025-10-20 22:37:31 +0530

    update logo

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export const Header = () => {
     return (
         <div className='w-full bg-cyan-950 flex items-center justify-between p-2'>
-            <div><Image src="/Images/Logo/logo1.png" alt="logo" width={80} height={0} className='rounded-full'/></div>
+            <div><Image src="/Images/Logo/logo2.png" alt="logo" width={80} height={0} className='rounded-full'/></div>
             <div className='w-[16rem] flex justify-center items-center gap-2'>
                 <Link href="/" className='p-1 text-amber-50'>Home</Link>
                 <Link href="/about" className='p-1 text-amber-50'>About</Link>
