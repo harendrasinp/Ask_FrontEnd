852d7eeb1d1e3cdd49f9fc897176ad7c3b6e0b12
Harendrasinh <haren111990@gmail.com>
2025-10-20 20:33:49 +0530

    chechupdate first

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export const Header = () => {
     return (
         <div className='w-[80%] flex items-center justify-between gap-4 border-2 border-amber-300 rounded-3xl px-6'>
-            <div><Image src="/Images/Logo/logo4.png" alt="logo" width={100} height={0} className='rounded-full'/></div>
+            <div><Image src="/Images/Logo/logo4.png" alt="logo" width={100} height={100} className='rounded-full'/></div>
             <div className='w-[25rem] flex justify-center items-center gap-6'>
                 <Link href="/" className='bg-blue-300 p-2 px-4 rounded-2xl'>Home</Link>
                 <Link href="/about"className='bg-blue-300 p-2 px-4 rounded-2xl'>About</Link>
