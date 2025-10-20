567a61b7dbe3ee1f7726f6a1f9cb5745ecfbf4a5
unknown <haren111990@gmail.com>
2025-10-20 22:19:41 +0530

    update final1

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     return (
         <div className='w-full bg-cyan-950 flex items-center justify-between p-2'>
             <div><Image src="/Images/Logo/logo1.png" alt="logo" width={80} height={0} className='rounded-full'/></div>
-            <div className='w-[15rem] flex justify-center items-center gap-2'>
+            <div className='w-[16rem] flex justify-center items-center gap-2'>
                 <Link href="/" className='p-1 text-amber-50'>Home</Link>
                 <Link href="/about" className='p-1 text-amber-50'>About</Link>
                 <Link href="/contact" className='p-1 text-amber-50'>Contact</Link>
