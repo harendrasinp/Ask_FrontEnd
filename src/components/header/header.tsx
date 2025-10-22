4426935fe774e745b06deecf19663211c838c906
unknown <haren111990@gmail.com>
2025-10-22 11:23:55 +0530

    update header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,11 +5,16 @@ export const Header = () => {
     return (
         <div className='w-full bg-cyan-950 flex items-center justify-between p-2'>
             <div><Image src="/Images/Logo/logo2.png" alt="logo" width={80} height={0} className='rounded-full'/></div>
-            <div className='w-[16rem] flex justify-center items-center gap-2'>
+            <div className='hidden w-[16rem] lg:flex justify-center items-center gap-2'>
                 <Link href="/" className='p-1 text-amber-50'>Home</Link>
                 <Link href="/about" className='p-1 text-amber-50'>About</Link>
                 <Link href="/contact" className='p-1 text-amber-50'>Contact</Link>
                 <Link href="/services" className='p-1 text-amber-50'>Services</Link>
+            </div>
+            <div className='lg:hidden flex flex-col justify-center items-center gap-1.5 border-1 border-amber-50 rounded p-2'>
+               <div className='w-[25px] h-[2px] bg-amber-50'></div>
+               <div className='w-[25px] h-[2px] bg-amber-50'></div>
+               <div className='w-[25px] h-[2px] bg-amber-50'></div>
             </div>
         </div>
     )
