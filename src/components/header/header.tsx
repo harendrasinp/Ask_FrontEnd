f2b8c6782f6362f6d866e31b3842f933d3c1ad9d
unknown <haren111990@gmail.com>
2025-10-22 11:27:57 +0530

    updated hambergur icon

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 export const Header = () => {
     return (
-        <div className='w-full bg-cyan-950 flex items-center justify-between p-2'>
+        <div className='w-full bg-gray-300 flex items-center justify-between p-2'>
             <div><Image src="/Images/Logo/logo2.png" alt="logo" width={80} height={0} className='rounded-full'/></div>
             <div className='hidden w-[16rem] lg:flex justify-center items-center gap-2'>
                 <Link href="/" className='p-1 text-amber-50'>Home</Link>
@@ -11,10 +11,10 @@ export const Header = () => {
                 <Link href="/contact" className='p-1 text-amber-50'>Contact</Link>
                 <Link href="/services" className='p-1 text-amber-50'>Services</Link>
             </div>
-            <div className='lg:hidden flex flex-col justify-center items-center gap-1.5 border-1 border-amber-50 rounded p-2'>
-               <div className='w-[25px] h-[2px] bg-amber-50'></div>
-               <div className='w-[25px] h-[2px] bg-amber-50'></div>
-               <div className='w-[25px] h-[2px] bg-amber-50'></div>
+            <div className='lg:hidden flex flex-col justify-center items-center gap-1.5 border-1 border-b-cyan-950 rounded p-2'>
+               <div className='w-[25px] h-[2px] bg-cyan-950'></div>
+               <div className='w-[25px] h-[2px] bg-cyan-950'></div>
+               <div className='w-[25px] h-[2px] bg-cyan-950'></div>
             </div>
         </div>
     )
