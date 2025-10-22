96427e8ffbae90c6ec1a54ff888870d497bb0bb5
Harendrasinh <haren111990@gmail.com>
2025-10-22 20:28:03 +0530

    update side bad menu

--- src/components/header_mobile/header.tsx
+++ src/components/header_mobile/header.tsx
@@ -1,12 +1,17 @@
 "use client"
-import React from 'react'
+import Link from 'next/link'
 import { useSelector } from 'react-redux'
 const HeaderMobile = () => {
     const status = useSelector((state: any) => state.counter.status);
   return (
-    <div className={`bg-cyan-950 h-screen w-full shadow-lg transform transition-trasform duration-300 z-50
+    <div className={`fixed bg-cyan-950 right-0 h-full w-64 shadow-lg transform transition-trasform duration-300 z-50
     ${status ? "translate-x-0" : "translate-x-full"}`}>
-        
+        <div className='flex flex-col items-center gap-4 mt-5'>
+            <Link href={"/"} className='text-white'>Home</Link>
+            <Link href={"/"} className='text-white'>Services</Link>
+            <Link href={"/"} className='text-white'>AboutUs</Link>
+            <Link href={"/"} className='text-white'>ContactUs</Link>
+        </div>
     </div>
   )
 }
