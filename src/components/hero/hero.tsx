3756f44796bf0601b3ff4e739f11231dbb7a572a
unknown <haren111990@gmail.com>
2025-10-23 21:16:51 +0530

    add heroimage

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+
 import { heroData } from '@/content/hero/data'
 const Hero = () => {
+  
   return (
-    <div className='relative flex justify-center items-center w-full h-screen'>
-      <Image src="/Images/Hero/bg-sky1.jpg" alt="logo" fill  className='object-cover'/>
-      <div className='absolute top-5 text-blue-500 font-bold text-[1rem] text-center'>
+    <div className='bg-[url("/Images/Hero/hero1.jpg")] bg-cover bg-no-repeat flex w-full 
+    h-[15rem] md:h-[25rem] lg:h-[25rem]'>
+      <div className='text-amber-50 font-semibold mt-2 text-[1rem] text-left'>
         <p className='w-[15rem]'>{heroData.heading}</p>
         </div>
     </div>
