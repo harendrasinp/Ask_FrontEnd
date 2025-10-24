17cb3f0d33bc2e6c1d18d32242d17704ca9cdee4
unknown <haren111990@gmail.com>
2025-10-24 14:38:52 +0530

    update photo shadow

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -25,17 +25,17 @@ const Hero = () => {
           <p className='w-[15rem] lg:w-full'>{heroData.heading}</p>
         </div>
       </div>
-      <div className='bg-[url("/Images/Hero/BgImage/herobg2.png")] bg-cover bg-center w-full h-screen flex flex-col justify-between items-center'>
+      <div className='bg-[url("/Images/Hero/BgImage/herobg2.png")] bg-cover bg-center w-full h-screen flex flex-col justify-baseline items-center gap-32'>
        
-       <div className='mt-2'>
+       <div className=''>
           <span className='text-green-600 font-bold geistSans'>{MissionData.title}</span>
-          <div className='w-[6.9rem] h-[0.2rem] bg-amber-400 mt-2 mb-2'></div>
+          <div className='w-[6.9rem] h-[0.2rem] bg-amber-400 mt-2'></div>
           <p className='text-black text-justify w-[23rem] lg:w-full'>{MissionData.Description}</p>
         </div>
 
-        <div className='relative flex justify-center w-[30rem] h-[15rem]'>
+        <div className='relative flex justify-center w-[18rem] h-[12rem] lg:w-[30rem] lg:h-[18rem]'>
           {ImagesData[index] ?
-            <Image src={ImagesData[index]} alt='installation Image'fill></Image>
+            <Image src={ImagesData[index]} alt='installation Image'fill className='shadow-2xl shadow-green-700'></Image>
             : null}
         </div>
 
