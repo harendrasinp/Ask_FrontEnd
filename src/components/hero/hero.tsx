f0c756be7bda175278966e8afb46759f5b6515d4
unknown <haren111990@gmail.com>
2025-10-25 13:17:33 +0530

    update desciption padding

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -11,16 +11,16 @@ const Hero = () => {
           <p className='w-[15rem] lg:w-full'>{heroData.heading}</p>
         </div>
       </div>
-      <div className='bg-[url("/Images/Hero/BgImage/herobg2.png")] bg-cover bg-center w-full h-screen flex flex-col justify-baseline items-center gap-32 lg:gap-60'>
-       
-       <div className=''>
+      <div className='bg-[url("/Images/Hero/BgImage/herobg2.png")] bg-cover bg-center w-full h-screen flex flex-col justify-baseline items-center gap-32 lg:gap-60 p-4'>
+
+        <div className=''>
           <span className='text-green-600 font-bold geistSans'>{MissionData.title}</span>
-          <div className='w-[6.9rem] h-[0.2rem] bg-amber-400 mt-2'></div>
-          <p className='text-black text-justify w-[23rem] lg:w-full'>{MissionData.Description}</p>
+          <div className='w-[6.9rem] h-[0.2rem] bg-amber-400 mb-2 '></div>
+          <p className='text-black text-justify w-full'>{MissionData.Description}</p>
         </div>
 
         <div className='relative flex justify-center w-[18rem] h-[12rem] lg:w-[30rem] lg:h-[18rem]'>
-            <video src="/Images/Hero/installation_pic/vedio.mp4" loop muted autoPlay playsInline ></video>
+          <video src="/Images/Hero/installation_pic/vedio.mp4" loop muted autoPlay playsInline ></video>
         </div>
 
       </div>
