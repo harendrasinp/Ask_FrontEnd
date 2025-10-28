a9ee48d3cee9330da0bcbfc1e6cb08734dbbd955
unknown <haren111990@gmail.com>
2025-10-28 22:11:25 +0530

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import ClientWrapper from "@/components/clientWraper/clientWraper";
+import { Poppins } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-poppins",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${poppins.variable}} antialiased`}>
       <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
