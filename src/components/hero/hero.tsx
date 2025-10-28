379bc5004c75d540b4ebe39340c7b3c9d12f8773
unknown <haren111990@gmail.com>
2025-10-28 22:01:13 +0530

    update line color

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <section className="relative w-full h-[15rem] lg:h-[20rem] flex items-center justify-center overflow-hidden">
         <img src="/Images/Hero/BgImage/hero1.jpg"alt="Hero background"
           className="absolute top-0 left-0 w-full h-full object-cover md:object-fill"/>
-        <div className='absolute text-amber-100 font-semibold text-[1rem] left-2 top-2 z-50'>
+        <div className='absolute text-blue-950 font-semibold text-[1rem] left-2 top-2 z-50'>
           <p className='w-[15rem] lg:w-full'>{heroData.heading}</p>
         </div>
       </section>
@@ -20,7 +20,7 @@ const Hero = () => {
         className='w-full h-full' />
         <div className='z-10 absolute w-[24rem] py-1 px-3 lg:w-[55rem] lg:p-5'>
           <span className='text-green-600 font-bold geistSans'>{MissionData.title}</span>
-          <div className='w-[6.9rem] h-[0.2rem] bg-amber-400 mb-2'></div>
+          <div className='w-[6.9rem] h-[0.2rem] bg-blue-950 mb-2'></div>
           <p className='text-black text-justify'>{MissionData.Description}</p>
         </div>
       </section>
@@ -31,7 +31,7 @@ const Hero = () => {
         </video>
         <div className='w-full px-4'>
           <span className='text-green-600 font-bold geistSans'>{whoweare.heading}</span>
-          <div className='w-[6.9rem] h-[0.2rem] bg-amber-400'></div>
+          <div className='w-[6.9rem] h-[0.2rem] bg-blue-950'></div>
           <p className='text-justify text-blue-400'>{whoweare.description}</p>
         </div>
       </section>
