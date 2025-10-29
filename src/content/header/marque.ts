d6423ea79726932c6b2ce1d86e39b3aee982e909
unknown <haren111990@gmail.com>
2025-10-29 23:12:55 +0530

    update marque icon

--- src/content/header/marque.ts
+++ src/content/header/marque.ts
@@ -1,5 +1,5 @@
 export const marqueData={
     name:"Direct Contact Mr.Amol Jamadar : ",
     Conatact:"+91-997-004-489-7",
-    email:"Email: lucky.rajput625@gmail.com"
+    email:"lucky.rajput625@gmail.com"
 }
