6b315205a134a430421707415fd286b833f01cce
unknown <haren111990@gmail.com>
2025-10-29 18:27:32 +0530

    update service cards

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { ServiceCard } from '@/content/services/servicesCard'
 const Services = () => {
   return (
     <div className='relative bg-orange-200/50 w-full h-fit flex flex-col justify-center items-center'>
@@ -8,11 +8,13 @@ const Services = () => {
         <img src="/Images/Services/solarbg.webp" alt="services background" className='w-full h-full'/>
       </div>
       <span className='text-blue-950 font-bold'>Services We Provide</span>
-      <div className='p-5'>
-        <div className='bg-cyan-950 w-fit h-fit p-2 rounded-2xl shadow-cyan-950 shadow-2xl'>
-          <h4 className='text-amber-50'>Solar Panals</h4>
-          <p className='text-amber-200'>We provide high quality solar panals for residential and commercial use.</p>
-        </div>
+      <div className='p-5 h-fit flex flex-col gap-8 justify-center items-center'>
+       {ServiceCard.map((service,index)=>(
+         <div key={index} className='bg-cyan-950 w-fit h-fit p-5 rounded-2xl shadow-cyan-950 shadow-2xl'>
+          <h4 className='text-amber-50'>{service.title}</h4>
+          <p className='text-amber-200'>{service.description}</p>
+        </div>))
+       }
       </div>
     </div>
   )
---
+++ src/content/services/servicesCard.ts
@@ -0,0 +1,23 @@
+export const ServiceCard=[
+    {   
+        title:"Solar Panels",
+        description:"We provide high quality solar panels for residential and commercial use.",
+        imageUrl:"/Images/Services/solarbg.webp"
+    },
+    {   
+        title:"Solar Panels",
+        description:"We provide high quality solar panels for residential and commercial use.",
+        imageUrl:"/Images/Services/solarbg.webp"
+    },
+    {   
+        title:"Solar Panels",
+        description:"We provide high quality solar panels for residential and commercial use.",
+        imageUrl:"/Images/Services/solarbg.webp"
+    },
+{   
+        title:"Solar Panels",
+        description:"We provide high quality solar panels for residential and commercial use.",
+        imageUrl:"/Images/Services/solarbg.webp"
+    },
+
+]
