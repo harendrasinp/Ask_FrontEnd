1fd4ff408ad99738c9ee54b98752cd156e00b478
unknown <haren111990@gmail.com>
2025-10-29 23:10:07 +0530

    update hero title

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <section className="relative w-full h-[15rem] lg:h-[20rem] flex items-center justify-center overflow-hidden">
         <img src="/Images/Hero/BgImage/hero1.jpg"alt="Hero background"
           className="absolute top-0 left-0 w-full h-full object-cover md:object-fill"/>
-        <div className='absolute text-blue-950 font-semibold text-[1rem] left-2 top-2 z-2'>
+        <div className='absolute text-white font-bold lg:font-semibold text-[1rem] left-2 top-2 z-2'>
           <p className='w-[15rem] lg:w-full font-semibold'>{heroData.heading}</p>
         </div>
       </section>
