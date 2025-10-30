d54682f356d7cff6f15621f7f08f1a0494de5039
unknown <haren111990@gmail.com>
2025-10-30 21:16:34 +0530

    update contact page

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Contact = () => {
     return (
-       <div className='w-full h-[45rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
-
+       <div className='bg-gray-600/50 w-full h-[45rem] lg:h-[55rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
+            <div className='relative'>
+                <h1 className='absolute top-11 left-8 text-shadow-yellow-800 text-[1.5rem]'>Contact Us</h1>
+            </div>
        </div>
     )
 }
