7035d12585dfcc11262d42c5b8802b61f8927726
unknown <haren111990@gmail.com>
2025-10-30 22:01:35 +0530

    update position in contact image

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 const Contact = () => {
     return (
-       <div className='bg-gray-600/50 w-full h-[45rem] lg:h-[55rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
-            <div className='relative'>
-                <h1 className='absolute top-8 right-[6rem] text-shadow-yellow-800 text-[1.5rem] lg:left-5'>Contact Us</h1>
+        <div className='bg-gray-600/50 w-full h-[45rem] lg:h-[55rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
+            <div className='flex justify-between items-center px-5 lg:p-10'>
+                <div className=''>
+                    <h1 className=' text-shadow-yellow-800 text-[1.5rem]'>Contact Us</h1>
+                </div>
+                <div className=''>
+                    <img src="/Images/Contact/contactTitle1.png" alt="Contact-title-logo"
+                        className='w-45 h-40' />
+                </div>
             </div>
-            <div className='relative'>
-                <img src="/Images/Contact/contactTitle1.png" alt="Contact-title-logo" 
-                className='absolute top-8 w-45 h-40 left-2 lg:right-[12rem] lg:w-[18rem] lg:h-[18rem]'/>
+
+            <div className='absolute w-full h-[35rem] p-4'>
+                <div className='w-full h-full bg-gray-100/15'>
+                </div>
             </div>
-            <div className='absolute w-full h-[45rem] p-4'>
-                <div className='w-full h-full bg-gray-100/15'></div>
-            </div>
-       </div>
+        </div>
     )
 }
 
