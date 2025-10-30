efd90b7897a9313223c199c38abe8d36ee812d9c
unknown <haren111990@gmail.com>
2025-10-30 20:55:50 +0530

    update contact bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import About from "@/components/about/About"
 import Hero from "@/components/hero/hero"
 import Services from "@/components/services/services"
+import Contact from "@/components/contact/Contact"
 const Home = () => {
   return (
     <>
      <Hero />
      <About/>
      <Services/>
+     <Contact/>
     </>
   )
 }
---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+       <div className='w-full h-[45rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
+
+       </div>
+    )
+}
+
+export default Contact
