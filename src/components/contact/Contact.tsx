4b3dae86338e04f516ce62231df7cdee527d7501
unknown <haren111990@gmail.com>
2025-10-30 22:44:28 +0530

    update final contact 2

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,55 +1,36 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone,faEnvelope,faUser,faLocationDot} from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faEnvelope, faUser, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 const Contact = () => {
-    return (
-        <div className='bg-gray-600/50 w-full h-[45rem] lg:h-[55rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
-            <div className='flex justify-between items-center px-5 lg:p-10'>
-                <div className=''>
-                    <h1 className=' text-shadow-yellow-800 text-[1.5rem]'>Contact Us</h1>
-                </div>
-                <div className=''>
-                    <img src="/Images/Contact/contactTitle1.png" alt="Contact-title-logo"
-                        className='w-45 h-40' />
-                </div>
-            </div>
+  return (
+    <div className='relative w-full min-h-screen bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
+      <div className='absolute inset-0 bg-black/50'></div>
 
-            <div className='w-full h-[35rem] p-4'>
-                <div className='w-full h-full bg-gray-100/15 flex flex-col justify-evenly p-5'>
-                   <div className=''>
-                     <ul className='flex flex-col justify-between items-start gap-2'>
-                        <li className='text-white'>
-                            <FontAwesomeIcon icon={faUser}/>
-                            Amol Jamadar
-                        </li>
-
-                        <li className='text-white'>
-                            <FontAwesomeIcon icon={faPhone}/>
-                            +91-997-004-489-7
-                        </li>
-
-                        <li className='text-white'>
-                            <FontAwesomeIcon icon={faEnvelope}/>
-                            amol_jamadar@asksolar.com
-                        </li>
-                        <li className='text-white'>
-                            <FontAwesomeIcon icon={faLocationDot}/>
-                            flat-12,wing2,shahada(Nandurbar)
-                        </li>
-                    </ul>
-                   </div>
-                   <div>
-                    <form className='flex flex-col gap-3'>
-                        <input type="text" placeholder='Your Name' className=' text-amber-50 p-2 rounded border border-amber-50 bg-amber-50/50'/>
-                        <input type="email" placeholder='Your Email' className='text-amber-50 p-2 rounded ounded border border-amber-50 bg-amber-50/50'/>
-                        <textarea placeholder='Your Message' className='text-amber-50 p-2 rounded h-24 ounded border border-amber-50 bg-amber-50/50'></textarea>
-                        <button type='submit' className='bg-blue-950 text-white p-2 rounded w-32'>Send Message</button>
-                    </form>
-                   </div>
-                </div>
-            </div>
+      <div className='relative z-10 p-6 flex flex-col gap-8'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-white text-3xl font-semibold'>Contact Us</h1>
+          <img src="/Images/Contact/contactTitle1.png" alt="Contact" className='w-20 h-20' />
         </div>
-    )
+
+        <div className='bg-white/10 backdrop-blur-md p-6 rounded-lg'>
+          <ul className='flex flex-col gap-2 text-white mb-4'>
+            <li><FontAwesomeIcon icon={faUser} className='mr-2' /> Amol Jamadar</li>
+            <li><FontAwesomeIcon icon={faPhone} className='mr-2' /> +91 9970044897</li>
+            <li><FontAwesomeIcon icon={faEnvelope} className='mr-2' /> amol_jamadar@asksolar.com</li>
+            <li><FontAwesomeIcon icon={faLocationDot} className='mr-2' /> Flat-12, Wing2, Shahada (Nandurbar)</li>
+          </ul>
+
+          <form className='flex flex-col gap-3'>
+            <input type="text" placeholder='Your Name' className='p-2 rounded border border-white bg-black/40 text-white placeholder-white' />
+            <input type="email" placeholder='Your Email' className='p-2 rounded border border-white bg-black/40 text-white placeholder-white' />
+            <textarea placeholder='Your Message' className='p-2 rounded border border-white bg-black/40 text-white placeholder-white h-24'></textarea>
+            <button type='submit' className='bg-blue-950 text-white p-2 rounded w-40 hover:bg-blue-800'>Send Message</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
