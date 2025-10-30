fdb8facfdb38c8a55b946656ae4a17ecd02a7b30
unknown <haren111990@gmail.com>
2025-10-30 21:30:42 +0530

    update contact title image

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,7 +4,11 @@ const Contact = () => {
     return (
        <div className='bg-gray-600/50 w-full h-[45rem] lg:h-[55rem] bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
             <div className='relative'>
-                <h1 className='absolute top-11 left-8 text-shadow-yellow-800 text-[1.5rem]'>Contact Us</h1>
+                <h1 className='absolute top-8 right-[6rem] text-shadow-yellow-800 text-[1.5rem]'>Contact Us</h1>
+            </div>
+            <div className='relative'>
+                <img src="/Images/Contact/contactTitle1.png" alt="Contact-title-logo" 
+                className='absolute top-8 w-45 h-40 lg:right-[12rem] lg:w[18rem] lg:h-[18rem]'/>
             </div>
        </div>
     )
