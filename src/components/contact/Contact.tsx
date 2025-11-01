165f2db02d752930b67034e46bc102807c808e2b
unknown <haren111990@gmail.com>
2025-11-01 20:53:24 +0530

    update contact padding

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,14 +7,14 @@ const Contact = () => {
     <div className='relative w-full min-h-screen bg-[url("/Images/Contact/contactBg1.png")] bg-cover bg-center bg-no-repeat'>
       <div className='absolute inset-0 bg-black/50'></div>
 
-      <div className='relative z-10 p-6 flex flex-col gap-8'>
+      <div className='relative z-10 p-4 flex flex-col gap-8'>
         <div className='flex justify-between items-center'>
           <h1 className='text-white text-3xl font-semibold'>Contact Us</h1>
           <img src="/Images/Contact/contactTitle1.png" alt="Contact" className='w-20 h-20' />
         </div>
 
         <div className='bg-white/10 backdrop-blur-md p-6 rounded-lg z-50'>
-          <ul className='flex flex-col gap-2 text-white mb-4'>
+          <ul className='flex flex-col gap-2 text-[1.3rem] text-white mb-4'>
             {ContactData.map((data,index)=>(
               <li key={index}><FontAwesomeIcon icon={data.iconData} className='mr-2 text-justify' />{data.detail}</li>
             ))}
